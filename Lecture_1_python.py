3c981a911dc5304ffa12f983d68981f95fc18c52
Bisma Raza <bismauniversity@gmail.com>
2025-12-23 19:39:51 +0500

    Question 1 - 3

--- Lecture_1_python.py
+++ Lecture_1_python.py
@@ -1,4 +1,29 @@
-a = 2
-b = 3
-sum = a+b 
-print("Sum")
+# Question 1
+# name = str(input("Enter Your Name:"))
+# age = int(input("Enter Your age:"))
+
+# print("Hello",name,",You are", age, "years Old!")
+
+#Question 2
+# a = int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+#Question 3
+# num1 = int(input("Enter first integer number:"))
+# num2 = int(input("Enter second integer number:"))
+# num3 = float(input("Enter first float number:"))
+
+# num1= float(num1)
+# num2= float(num2)
+
+# average= (num1 +num2 +num3)/3
+
+# print("The average of three numbers is:", average)
+
+#Question 4
+
