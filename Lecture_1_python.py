5eb4ec88adfe8a755edce73c6b092bf6a7528cb1
Bisma Raza <bismauniversity@gmail.com>
2025-12-23 19:48:55 +0500

    Question 1 - 4

--- Lecture_1_python.py
+++ Lecture_1_python.py
@@ -1,29 +1,37 @@
 # Question 1
-# name = str(input("Enter Your Name:"))
-# age = int(input("Enter Your age:"))
+name = str(input("Enter Your Name:"))
+age = int(input("Enter Your age:"))
 
-# print("Hello",name,",You are", age, "years Old!")
+print("Hello",name,",You are", age, "years Old!")
 
 #Question 2
-# a = int(input("Enter first number:"))
-# b = int(input("Enter second number:"))
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
 
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print(a/b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
 
 #Question 3
-# num1 = int(input("Enter first integer number:"))
-# num2 = int(input("Enter second integer number:"))
-# num3 = float(input("Enter first float number:"))
+num1 = int(input("Enter first integer number:"))
+num2 = int(input("Enter second integer number:"))
+num3 = float(input("Enter first float number:"))
 
-# num1= float(num1)
-# num2= float(num2)
+num1= float(num1)
+num2= float(num2)
 
-# average= (num1 +num2 +num3)/3
+average= (num1 +num2 +num3)/3
 
-# print("The average of three numbers is:", average)
+print("The average of three numbers is:", average)
 
 #Question 4
+num1 = input("Enter first number:")
 
+num2=int(num1)
+num3=float(num1)
+num4=str(num1)
+
+print(num2, type(num2))
+print(num3, type(num3))
+print(num4, type(num4))
