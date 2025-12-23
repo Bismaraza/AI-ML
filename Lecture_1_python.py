7568e29fc16200735e8c976cfadc0c34df25829f
Bisma Raza <bismauniversity@gmail.com>
2025-12-23 20:33:18 +0500

    Question 1 - 6

--- Lecture_1_python.py
+++ Lecture_1_python.py
@@ -35,3 +35,28 @@ num4=str(num1)
 print(num2, type(num2))
 print(num3, type(num3))
 print(num4, type(num4))
+
+
+# Question 5 
+
+x = 10 + 3* 2 **2
+# step 1: 2 ** 2 = 4
+# step 2: 3 * 4 = 12
+# step 3: 10 + 12 = 22
+print(x)
+
+#Question 6
+a = 10
+b = 5
+c =12
+
+swap = a
+a = b
+b =c
+c = swap
+
+print("The value of a after swapping:", a)
+print("The value of b after swapping:", b)
+print("The value of c after swapping:", c)
+
+
