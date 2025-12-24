3b43314397221cf4dce18ff683bdd2cce93b501d
Bisma Raza <bismauniversity@gmail.com>
2025-12-24 13:09:34 +0500

    Lecture Question 1-3

---
+++ Lecture_2.py
@@ -0,0 +1,33 @@
+# # Question 1
+
+# age = int(input(" Enter your age:"))
+
+# if age>= 18:
+#     print("You can Vote")
+   
+# else:
+#     print("You cannot Vote")
+
+
+# print("Thank you")
+
+# # Question 2 
+
+# color= input("Enter the color of the traffic light:")
+# if color == "Red":
+#     print("Stop")
+# elif color == "Yellow":
+#     print("Wait")
+# elif color == "Green":
+#     print("Go")
+# else: 
+#     print("Invalid color")
+
+# # Question 3 
+# age = int(input("Enter your age:"))
+# if age <= 13:
+#     print("Child")
+# elif age > 13 and age < 18:
+#     print("Teenager")
+# else:
+#     print("Adult")
