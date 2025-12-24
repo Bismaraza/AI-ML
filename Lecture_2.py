8e9a97c73bd4acc25468b9f415858ca00f25f290
Bisma Raza <bismauniversity@gmail.com>
2025-12-24 13:25:27 +0500

    Question 4

--- Lecture_2.py
+++ Lecture_2.py
@@ -1,33 +1,45 @@
-# # Question 1
+# Question 1
 
-# age = int(input(" Enter your age:"))
+age = int(input(" Enter your age:"))
 
-# if age>= 18:
-#     print("You can Vote")
+if age>= 18:
+    print("You can Vote")
    
-# else:
-#     print("You cannot Vote")
+else:
+    print("You cannot Vote")
 
 
-# print("Thank you")
+print("Thank you")
 
-# # Question 2 
+# Question 2 
 
-# color= input("Enter the color of the traffic light:")
-# if color == "Red":
-#     print("Stop")
-# elif color == "Yellow":
-#     print("Wait")
-# elif color == "Green":
-#     print("Go")
-# else: 
-#     print("Invalid color")
+color= input("Enter the color of the traffic light:")
+if color == "Red":
+    print("Stop")
+elif color == "Yellow":
+    print("Wait")
+elif color == "Green":
+    print("Go")
+else: 
+    print("Invalid color")
 
-# # Question 3 
-# age = int(input("Enter your age:"))
-# if age <= 13:
-#     print("Child")
-# elif age > 13 and age < 18:
-#     print("Teenager")
-# else:
-#     print("Adult")
+# Question 3 
+age = int(input("Enter your age:"))
+if age <= 13:
+    print("Child")
+elif age > 13 and age < 18:
+    print("Teenager")
+else:
+    print("Adult")
+
+# Question 4
+
+username = input("Enter your username:")
+password = input("Enter your password:")
+
+if (username == "admin" and password == "pass"):
+    print("Login successful")
+elif (username != "admin"):
+    print("Wrong username")
+else:
+    print("Wrong password")
