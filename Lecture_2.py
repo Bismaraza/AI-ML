31a6276db3332df438dedc149374d2434f55f7b6
Bisma Raza <bismauniversity@gmail.com>
2025-12-29 17:27:58 +0500

    Chap 2 DONE

--- Lecture_2.py
+++ Lecture_2.py
@@ -1,165 +1,165 @@
-# # Conditional Statements Exercises 
-# # Question 1
+# Conditional Statements Exercises 
+# Question 1
 
-# age = int(input(" Enter your age:"))
+age = int(input(" Enter your age:"))
 
-# if age>= 18:
-#     print("You can Vote")
+if age>= 18:
+    print("You can Vote")
    
-# else:
-#     print("You cannot Vote")
+else:
+    print("You cannot Vote")
 
 
-# print("Thank you")
+print("Thank you")
 
-# # Question 2 
+# Question 2 
 
-# color= input("Enter the color of the traffic light:")
-# if color == "Red":
-#     print("Stop")
-# elif color == "Yellow":
-#     print("Wait")
-# elif color == "Green":
-#     print("Go")
-# else: 
-#     print("Invalid color")
+color= input("Enter the color of the traffic light:")
+if color == "Red":
+    print("Stop")
+elif color == "Yellow":
+    print("Wait")
+elif color == "Green":
+    print("Go")
+else: 
+    print("Invalid color")
 
-# # Question 3 
-# age = int(input("Enter your age:"))
-# if age <= 13:
-#     print("Child")
-# elif age > 13 and age < 18:
-#     print("Teenager")
-# else:
-#     print("Adult")
+# Question 3 
+age = int(input("Enter your age:"))
+if age <= 13:
+    print("Child")
+elif age > 13 and age < 18:
+    print("Teenager")
+else:
+    print("Adult")
 
-# # Question 4
+# Question 4
 
-# username = input("Enter your username:")
-# password = input("Enter your password:")
+username = input("Enter your username:")
+password = input("Enter your password:")
 
-# if (username == "admin" and password == "pass"):
-#     print("Login successful")
-# elif (username != "admin"):
-#     print("Wrong username")
-# else:
-#     print("Wrong password")
-
-# # # Question 5
-
-# number = int(input("Enter a number:"))
-
-# if (number % 2 == 0):
-#     print("Even")
-# else:
-#     print("ODD")
-
-# # # Question 6 
-# age = 12
-# status = "Adult" if age >=20 else "Not Adult"
-# print(status)
-
-
-
-#  #Loops Exercises
-# Count = 10
-# while (Count >= 1):
-#     print(Count)
-#     Count -= 1
-
-# print("After Loop Count = ", Count)
-
-# # Question 2 
-
-# ## Loops Exercises
-# Count = 0
-# while (Count < 10):
-#     print(Count)
-#     Count += 1
-
-# # In majority cases, We start i from 0 and the sign will be < 
-
-# # Question 3 
-# i = 0 
-# while i<10: 
-#     print( 6 * (i+1))
-#     i = i+1 
-
-# # Question 4
-# i = 1
-# while i<=10: 
-#     if( i % 6 ==0):
-#         break
-#     print(i)
-#     i = i+1 
+if (username == "admin" and password == "pass"):
+    print("Login successful")
+elif (username != "admin"):
+    print("Wrong username")
+else:
+    print("Wrong password")
 
 # # Question 5
-# i = 1
-# while i<=10: 
-#     if( i % 3 ==0):
-#         i = i+1
-#         continue
-#     print(i)
-#     i = i+1 
 
-# print("End of Loop")
+number = int(input("Enter a number:"))
 
-# # Question 6
-# i = 0
-# while i< 10: 
-#     i = i+1
-#     if( i % 2 ==0):
-#         continue
-#     print(i)
+if (number % 2 == 0):
+    print("Even")
+else:
+    print("ODD")
 
-# # For Loop Exercise
-# # Qustion 1 
-
-# for i in range(5):
-#     print("Hello World")
-
-# # Question 2
-# word = "Artiaeiouficial"
-
-# count = 0
-# for ch in word:
-#     if(ch == 'a' or ch == 'A' or ch == 'i' or ch == 'I' or ch == 'o' or ch == 'O' or ch == 'e' or ch == 'E' or ch == 'u' or ch == 'U'):
-#         count = count + 1
-# print( count)
+# # Question 6 
+age = 12
+status = "Adult" if age >=20 else "Not Adult"
+print(status)
 
 
-# # Question 3
-# for i in range( 1, 10):
-#     print(i)
 
-# # Question 4
-# sum =0 
-# for i in range( 1, 6):
-#     sum = sum + i
-# print( sum)
+ #Loops Exercises
+Count = 10
+while (Count >= 1):
+    print(Count)
+    Count -= 1
 
-# # Question 5 
-# number = input(" Enter a number:")
-# i=1
-# while i<=10:
-#     print( int(number)*i)
-#     i = i+1
+print("After Loop Count = ", Count)
 
- # # Question 6
-# i = 0
-# while i<10:
-#     i = i+1 
-#     if (i % 2 ==0):
-#         continue
-#     print(i)
+# Question 2 
 
-## Funcations Exercises
-## Question 1
-# def hello():
-#     print("Hello World")
+## Loops Exercises
+Count = 0
+while (Count < 10):
+    print(Count)
+    Count += 1
 
-# hello()
-# hello()
+# In majority cases, We start i from 0 and the sign will be < 
+
+# Question 3 
+i = 0 
+while i<10: 
+    print( 6 * (i+1))
+    i = i+1 
+
+# Question 4
+i = 1
+while i<=10: 
+    if( i % 6 ==0):
+        break
+    print(i)
+    i = i+1 
+
+# Question 5
+i = 1
+while i<=10: 
+    if( i % 3 ==0):
+        i = i+1
+        continue
+    print(i)
+    i = i+1 
+
+print("End of Loop")
+
+# Question 6
+i = 0
+while i< 10: 
+    i = i+1
+    if( i % 2 ==0):
+        continue
+    print(i)
+
+# For Loop Exercise
+# Qustion 1 
+
+for i in range(5):
+    print("Hello World")
+
+# Question 2
+word = "Artiaeiouficial"
+
+count = 0
+for ch in word:
+    if(ch == 'a' or ch == 'A' or ch == 'i' or ch == 'I' or ch == 'o' or ch == 'O' or ch == 'e' or ch == 'E' or ch == 'u' or ch == 'U'):
+        count = count + 1
+print( count)
+
+
+# Question 3
+for i in range( 1, 10):
+    print(i)
+
+# Question 4
+sum =0 
+for i in range( 1, 6):
+    sum = sum + i
+print( sum)
+
+# Question 5 
+number = input(" Enter a number:")
+i=1
+while i<=10:
+    print( int(number)*i)
+    i = i+1
+
+ # Question 6
+i = 0
+while i<10:
+    i = i+1 
+    if (i % 2 ==0):
+        continue
+    print(i)
+
+# Funcations Exercises
+# Question 1
+def hello():
+    print("Hello World")
+
+hello()
+hello()
  
 # Question 2 
 def avg(a, b, c = 2):
@@ -168,3 +168,15 @@ def avg(a, b, c = 2):
 
 result = avg( 1,2)
 print( result)
+
+# Question 3 
+
+def calculate_factorial(n):
+    fact = 1 
+    for i in range( 1, n+1):
+        fact = fact * i 
+    return fact
+
+n = int(input(" Enter a number:"))
+result = calculate_factorial( n) 
+print(" Factorial is:", result)
