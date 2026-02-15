dfe15a616d47bdd1ed1a68122f8a4e913697764d
Bisma Raza <bismauniversity@gmail.com>
2026-02-15 19:57:19 +0500

    Update Lecture_3_Practice_Questions.py

--- Lecture_3_Practice_Questions.py
+++ Lecture_3_Practice_Questions.py
@@ -1,12 +1,23 @@
-## String is Palindrome or not
-text = "madam"
-reverse_string = ""
-idx=0
+# # String is Palindrome or not
+# text = "madam"
+# reverse_string = ""
+# idx=0
 
-for ch in text:  
-    reverse_string = ch + reverse_string 
-print(reverse_string)
-if(reverse_string == text):
-    print("Pilandrome")
-else:
-    print("Not")
+# for ch in text:  
+#     reverse_string = ch + reverse_string 
+# print(reverse_string)
+# if(reverse_string == text):
+#     print("Pilandrome")
+# else:
+#     print("Not")
+
+
+## Average f numbers in list
+# my_list = [1,2,3,4,5]
+# sum = 0
+# for num in my_list:
+#     sum = sum+ num
+#     print(sum)
+
+# average = sum / len(my_list)
+# print(average)
